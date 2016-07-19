e3fdbb178a973e95b16aba7a4f932e29d94d9225
Kanstantsin <iotch@users.noreply.github.com>
2016-07-19 18:21:46 +0300

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -29,7 +29,7 @@ const historyPlugin = ({ forceDeactivate } = {}) => (router) => {
         if (!state) {
             // If current state is already the default route, we will have a double entry
             // Navigating back and forth will emit SAME_STATES error
-            router.navigate(defaultRoute, defaultParams, {forceDeactivate, reload: true, replace: true});
+            defaultRoute && router.navigate(defaultRoute, defaultParams, {forceDeactivate, reload: true, replace: true});
             return;
         }
         if (router.lastKnownState && router.areStatesEqual(state, router.lastKnownState, false)) {
